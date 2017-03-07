87e6f5eb2325f06042c4529b354b7991c81ee62c
Bruce Grugett <bgrugett@gmail.com>
2017-03-07 10:47:24 -0500

    kenny start

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
 
-app.get('/', function(req, resp, next) {
-  resp.send('Welcome');
+//this caputres all requests must be first and call next
+app.use(function(req, res, next) {
+  console.log('I just heard', req);
   next();
 });
 
+app.get('/', function(req, resp, next) {
+  resp.send('Welcome');
+});
+
+app.get('/news', function(req, resp, next) {
+  resp.send('Welcome to the news');
+});
